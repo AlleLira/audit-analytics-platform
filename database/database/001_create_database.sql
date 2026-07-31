a9220dc741ed77524af43eab60fc17afaaffe10b
Alessandra Lira <105083697+AlleLira@users.noreply.github.com>
2026-07-31 14:18:51 -0300

    Update 001_create_database.sql

--- database/database/001_create_database.sql
+++ database/database/001_create_database.sql
@@ -2,20 +2,30 @@
 ===============================================================================
 Projeto.....: Audit Analytics Platform
 Arquivo.....: 001_create_database.sql
-Objetivo....: Criação do banco de dados AuditAnalyticsDW
+Objetivo....: Criar o banco de dados AuditAnalyticsDW
 Autor.......: Alessandra Lira
 Versão......: 1.0
-Data.........: 31/07/2026
+Data........: 31/07/2026
 ===============================================================================
 */
+
+USE master;
+GO
 
 IF DB_ID('AuditAnalyticsDW') IS NULL
 BEGIN
     CREATE DATABASE AuditAnalyticsDW;
+    PRINT 'Banco AuditAnalyticsDW criado com sucesso.';
+END
+ELSE
+BEGIN
+    PRINT 'O banco AuditAnalyticsDW já existe.';
 END;
 GO
 
 USE AuditAnalyticsDW;
 GO
 
-SELECT DB_NAME() AS BancoAtual;
+SELECT
+    DB_NAME() AS BancoAtual;
+GO
